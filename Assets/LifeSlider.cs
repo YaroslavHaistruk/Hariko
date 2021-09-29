1628f4c29aa355baa461fd9e6a1e0ff9b27ef7bb
YaroslavHaistruk <gaystruko1@gmail.com>
2021-09-29 16:11:09 +0200

    Create LifeSlider.cs

---
+++ Assets/LifeSlider.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeSlider : MonoBehaviour
+{
+	public Pers pers;
+    public Image fillImage;
+	private Slider sldr;
+	
+    void Awake()
+	{
+		sldr = GetComponent<Slider>();
+	}
+
+    
+    void Update()
+    {
+		if(sldr.value <= sldr.minValue)
+		{
+			fillImage.enabled = false;
+		}
+		
+		if(sldr.value > sldr.minValue && !fillImage.enabled)
+		{
+			fillImage.enabled = true;
+		}
+		
+		float fValue = pers.PersLife;
+		
+		if(fValue == 2)
+		{
+			fillImage.color = Color.yellow;
+		}
+		else if(fValue > sldr.maxValue / 2)
+		{
+			fillImage.color = Color.green;
+		}
+		else if(fValue == 1)
+		{
+			fillImage.color = Color.red;
+		}
+		sldr.value = fValue;
+		
+    }
+}
