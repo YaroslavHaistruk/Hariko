8772e63e5c5d8ea1e22c308176ee16edb012175c
YaroslavHaistruk <gaystruko1@gmail.com>
2021-09-29 16:11:44 +0200

    Create Tportal.cs

---
+++ Assets/Tportal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tportal : MonoBehaviour
+{
+	public Transform zmpoz;
+	public GameObject pers;
+	public int x;
+	public int y;
+	
+	void OnCollisionEnter2D(Collision2D cl)
+    {
+		if (cl.gameObject.tag == "Player")
+        {
+			pers.transform.position = zmpoz.position + new Vector3(x,y);
+        }
+    }
+}
